f0077e750b9d8015d15e271ff8d01bec3771b8ea
Cheddar2012 <cheddar1@outlook.com>
2015-04-21 00:13:04 -0700

    Added killable bad guy
    Left click to attack - 5 hits to kill

---
+++ Assets/Scripts/EnemyStats.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyStats : MonoBehaviour {
+
+	public float maxHp = 50;
+	private float curHp;
+
+	// Use this for initialization
+	void Start () {
+		curHp = maxHp;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void TakeDamage (int damage) {
+		curHp = Mathf.Max(curHp - damage, 0);
+		if (curHp == 0) {
+			Death ();
+		}
+	}
+
+	void Death () {
+		Destroy(gameObject);
+	}
+}
--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -21,6 +21,11 @@ public class Inventory : MonoBehaviour {
 				AddPickupToInventory();
 			}
 		}
+
+		if ( Input.GetKeyDown(KeyCode.Mouse0) ) {
+			Debug.Log ("Attacking");
+			AttackEnemy();
+		}
 	}
 
 	bool LookingAtPickup () {
@@ -39,6 +44,16 @@ public class Inventory : MonoBehaviour {
 			interactionDescriptorText.text = "";
 		}
 		return false;
+	}
+
+	void AttackEnemy () {
+		Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit, 3)) {
+			if (hit.collider.gameObject.tag == "Enemy") {
+				hit.collider.gameObject.GetComponent<EnemyStats>().TakeDamage(10);
+			}
+		}
 	}
 
 	void AddPickupToInventory() {
