0370c6759dccde443a27ce71c6346e83a4e9dad6
Cheddar2012 <cheddar1@outlook.com>
2015-11-08 17:13:22 -0800

    Added title screen, tutorial scene, and pause menu.
    Consolidated load level and pause game functionality. New prefab Pause
    Menu needs to be dropped into Canvas for every new scene. Pause script
    on Player requires reference to Pause Menu, and OnClick functions for
    Pause Menu buttons require references to Player scripts.

---
+++ Assets/Scripts/Game/GoToScene.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToScene : MonoBehaviour {
+
+    /// <summary>
+    /// Detects when the player collides with an object and loads a level if the object
+    /// has a LoadScene tag.
+    /// </summary>
+    /// <param name="hit">The object collided with.</param>
+	void OnControllerColliderHit (ControllerColliderHit hit) {
+        // Check if the object has a LoadScene tag
+		if (hit.gameObject.tag == "LoadScene") {
+            // Load the scene with the same name as the LoadScene game object
+            LoadLevel(hit.gameObject.name);
+		}
+	}
+
+    /// <summary>
+    /// Loads a scene or quits the game.
+    /// </summary>
+    public void LoadLevel(string levelName)
+    {
+        if (levelName == "Quit")
+        {
+            Application.Quit();
+        }
+        else
+        {
+            Application.LoadLevel(levelName);
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Pause.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+
+    public GameObject playerCamera;
+    public GameObject pauseMenu;
+
+	void Start () {
+        // Hide the pause menu on initialization
+        pauseMenu.SetActive(false);
+
+        // Make sure the game is unpaused on initialization
+        Time.timeScale = 1;
+	}
+
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.Escape)) {
+            TogglePause();
+        }
+	}
+
+    /// <summary>
+    /// Pauses/unpauses the gameplay and shows/hides the pause menu.
+    /// </summary>
+    public void TogglePause()
+    {
+        // Toggle the timescale between 1 and 0
+        Time.timeScale = (Time.timeScale + 1) % 2;
+
+        // Toggle MouseLook scripts on both Player and Main Camera
+        GetComponent<MouseLook>().enabled = !GetComponent<MouseLook>().enabled;
+        playerCamera.GetComponentInChildren<MouseLook>().enabled = !playerCamera.GetComponentInChildren<MouseLook>().enabled;
+
+        // Toggle the pause menu on or off
+        pauseMenu.SetActive(!pauseMenu.activeSelf);
+    }
+}
