d50143ded167e83e8a715772ed2b060d694dfb8b
Cheddar2012 <cheddar1@outlook.com>
2015-12-07 22:48:26 -0800

    Allow debug text toggle with back quote/tilde key.
    Also moved around some folders.

---
+++ Assets/Scripts/Debug/ShowDebugPanel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowDebugPanel : MonoBehaviour {
+
+    GameObject debugPanel;
+
+	// Use this for initialization
+	void Start () {
+        debugPanel = GameObject.Find("DebugPanel");
+        debugPanel.SetActive(Application.isEditor);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.BackQuote)) {
+            debugPanel.SetActive(!debugPanel.activeSelf);
+        }
+	}
+}
