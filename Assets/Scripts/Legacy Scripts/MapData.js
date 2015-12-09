132fd54186481ff600929162edb1aa1800a6f55c
Cheddar2012 <cheddar1@outlook.com>
2015-12-08 21:19:36 -0800

    Removed legacy scripts and textures

--- Assets/Scripts/Legacy Scripts/MapData.js
+++
@@ -1,2 +0,0 @@
-﻿static var mapMatrix : int[,] = new int[10,10];
-static var mapCreated : boolean = false;
--- Assets/Scripts/Legacy Scripts/TravelData.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class TravelData : MonoBehaviour {
-
-	public static string level;
-	public static Vector3 playerSpawnPosition = new Vector3(0,0,0);
-	public static Quaternion playerSpawnRotation;
-}
